890af4d47d68c244e7ca7b42177d662b786aad23
matias-gonz <maigonzalez@fi.uba.ar>
2024-06-13 15:24:54 +0200

    Use generic modulus

--- src/mega_lehmer.rs
+++ src/mega_lehmer.rs
@@ -1,19 +1,19 @@
 use crate::felt::Felt;
 
-pub struct MegaLehmer {
-    multiplier: Felt,
-    last_gen: Felt,
+pub struct MegaLehmer<const MODULUS: u64> {
+    multiplier: Felt<MODULUS>,
+    last_gen: Felt<MODULUS>,
 }
 
-impl MegaLehmer {
-    pub fn new(seed: Felt, multiplier: Felt) -> MegaLehmer {
+impl<const MODULUS: u64> MegaLehmer<MODULUS> {
+    pub fn new(seed: Felt<MODULUS>, multiplier: Felt<MODULUS>) -> MegaLehmer<MODULUS> {
         MegaLehmer {
             multiplier,
             last_gen: seed,
         }
     }
 
-    pub fn gen(&mut self) -> Felt {
+    pub fn gen(&mut self) -> Felt<MODULUS> {
         self.last_gen = self.last_gen * self.multiplier;
         self.last_gen
     }
@@ -23,11 +23,13 @@ impl MegaLehmer {
 mod tests {
     use super::*;
 
+    type Felt17 = Felt<17>;
+
     #[test]
     fn test_gen() {
-        let mut lehmer = MegaLehmer::new(Felt::new(1), Felt::new(2));
-        assert_eq!(lehmer.gen(), Felt::new(2));
-        assert_eq!(lehmer.gen(), Felt::new(4));
-        assert_eq!(lehmer.gen(), Felt::new(8));
+        let mut lehmer = MegaLehmer::new(Felt17::new(1), Felt17::new(2));
+        assert_eq!(lehmer.gen(), Felt17::new(2));
+        assert_eq!(lehmer.gen(), Felt17::new(4));
+        assert_eq!(lehmer.gen(), Felt17::new(8));
     }
 }
