25febccb931579307f1a664399bd24ebd33dd180
Cold_Blooded_ <neorobert123@gmx.de>
2024-06-27 14:25:05 +0200

    cleanup comments, remove struct from seedgen

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-mod felt;
-mod mega_lehmer;
-mod seed_generators;
-
-fn main() {
-    println!("Hello, world!");
-}
--- src/mega_lehmer.rs
+++ src/mega_lehmer.rs
@@ -24,17 +24,17 @@ mod tests {
     use super::*;
     use crate::felt::Felt;
 
-    type FeltX = Felt<17>;
+    type Felt17 = Felt<17>;
 
     #[test]
     fn test_gen() {
-        let mut lehmer = MegaLehmer::new(FeltX::new(1), FeltX::new(2));
-        assert_eq!(lehmer.gen(), FeltX::new(2));
-        assert_eq!(lehmer.gen(), FeltX::new(4));
-        assert_eq!(lehmer.gen(), FeltX::new(8));
-        assert_eq!(lehmer.gen(), FeltX::new(16));
-        assert_eq!(lehmer.gen(), FeltX::new(15));
-        assert_eq!(lehmer.gen(), FeltX::new(13));
-        assert_eq!(lehmer.gen(), FeltX::new(9));
+        let mut lehmer = MegaLehmer::new(Felt17::new(1), Felt17::new(2));
+        assert_eq!(lehmer.gen(), Felt17::new(2));
+        assert_eq!(lehmer.gen(), Felt17::new(4));
+        assert_eq!(lehmer.gen(), Felt17::new(8));
+        assert_eq!(lehmer.gen(), Felt17::new(16));
+        assert_eq!(lehmer.gen(), Felt17::new(15));
+        assert_eq!(lehmer.gen(), Felt17::new(13));
+        assert_eq!(lehmer.gen(), Felt17::new(9));
     }
 }
